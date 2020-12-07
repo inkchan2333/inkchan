bd948473d92c600329729ea68a81ca43cea0a651
inkchan2333 <51058019+inkchan2333@users.noreply.github.com>
2020-12-07 16:42:25 +0800

    Update ThreadBase.cpp

--- ThreadBase.cpp
+++ ThreadBase.cpp
@@ -1,29 +1,18 @@
 #include "ThreadBase.h"
 
-ThreadBase::ThreadBase()
-{
-	m_thread = NULL;
-}
+ThreadBase::ThreadBase(){
+	m_thread = NULL;}
+ThreadBase::~ThreadBase(){}
 
-ThreadBase::~ThreadBase()
-{
-}
-
-void ThreadBase::start()
-{
+void ThreadBase::start(){
 	if (m_thread == NULL)
-	{
 		m_thread = new std::thread{ &ThreadBase::threadMain,this};
-	}
+
 }
 
-void ThreadBase::quit()
-{
+void ThreadBase::quit(){
 	if (m_thread!=nullptr)
-	{
-		delete m_thread;
-		m_thread = nullptr;
-	}
+	{delete m_thread;m_thread = nullptr;}
 }
 
 void ThreadBase::mSleep(int64_t m)
